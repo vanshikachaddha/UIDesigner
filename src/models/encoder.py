1fd5f17f1ed51e70e98b8fba6e19e72547796ef3
Vanshika Chaddha <vchaddha@bu.edu>
2025-11-30 17:06:22 -0500

    added encoder

--- src/models/encoder.py
+++ src/models/encoder.py
@@ -16,35 +16,52 @@ class CNNEncoder(nn.Module):
     - 3 conv blocks (conv + ReLU + maxpool)
     - Global average pool at the end
     """
+    def __init__(self):
+        super(CNNEncoder, self).__init__()
+        self.conv1 = nn.Conv2d(3,32,3, padding=1)
+        self.conv2 = nn.Conv2d(32, 32, 3, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
+        self.conv4 = nn.Conv2d(64, 64, 3, padding=1)
+        self.conv5 = nn.Conv2d(64, 128, 3, padding=1)
+        self.conv6 = nn.Conv2d(128, 128, 3, padding=1)
+        self.relu = nn.ReLU()
+        self.pool = nn.MaxPool2d(2)
+        self.f1 = nn.Linear(in_features=32*32*128, out_features=1024)
+        self.f2 = nn.Linear(in_features=1024, out_features=1024)
+    def block1(self, input):
+        # input (256, 256, 3)
+        conv1 = self.relu(self.conv1(input))
+        # conv1 (256, 256, 32)
+        conv2 = self.relu(self.conv2(conv1))
+        # conv2 (256, 256, 32)
+        self.b1_result =self.pool(conv2)
+        # max pool (128, 128, 32)
+        return self.b1_result
+    def block2(self, input):
+        conv3 = self.relu(self.conv3(self.b1_result))
+        # conv3 (128, 128, 64)
+        conv4 = self.relu(self.conv4(conv3))
+        # conv4 (128, 128, 64)
+        self.b2_result = self.pool(conv4)
+        # max pool (64, 64, 64)
+        return self.b2_result
+    def block3(self, input):
+        conv5 = self.relu(self.conv5(self.b2_result))
+        # conv5 (64, 64, 128)
+        conv6 = self.relu(self.conv6(conv5))
+        # conv5 (64, 64, 128)
+        self.b3_result = self.pool(conv6)
+        # max pool (32, 32, 128)
+        return self.b3_result
+    def forward(self, x):
+        block1_output = self.block1(x)
+        block2_output = self.block2(block1_output)
+        block3_output = self.block3(block2_output)
+        f1_output = self.relu(self.f1(block3_output))
+        f2_output = self.relu(self.f2(f1_output))
+        return f2_output
 
-    def __init__(self, out_dim: int = 512):
-        super().__init__()
-        self.features = nn.Sequential(
-            # Block 1
-            nn.Conv2d(3, 32, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2),  # 112x112
 
-            # Block 2
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2),  # 56x56
 
-            # Block 3
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2),  # 28x28
-        )
 
-        self.global_pool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(128, out_dim)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """
-        x: (B, 3, H, W) -> (B, out_dim)
-        """
-        x = self.features(x)
-        x = self.global_pool(x)         # (B, 128, 1, 1)
-        x = x.view(x.size(0), -1)       # (B, 128)
-        x = self.fc(x)                  # (B, out_dim)
-        return x
+    
